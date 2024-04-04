a9357e1ad1198022fe4e5bea002ebd9e65331af8
andreibegu <abegu91@gmail.com>
2024-04-04 16:30:45 +0200

    Fix svh unit (again)

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { getCurrentGame } from "@/lib/data";
 
 export default async function Home() {
   return (
-    <main className="flex flex-col flex-1 h-svh justify-center items-center gap-5">
+    <main className="flex flex-col flex-1 h-full justify-center items-center gap-5">
       <Item />
       <Game id={await getCurrentGame()} />
     </main >
